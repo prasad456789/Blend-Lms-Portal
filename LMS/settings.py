3d7cfe3de2bf347a393ab65e637586ff0650a312
prasad456789 <iiittstudent755@gmail.com>
2024-10-18 17:02:52 +0530

    Update settings.py

--- LMS/settings.py
+++ LMS/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-0ks-8)i(9#dzlvx9)ze^%t#ulgh0h2s#e&jay0nh6^y!qf5ok%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['43.204.228.70']
+ALLOWED_HOSTS = ['43.204.228.70','http://127.0.0.1:8000/']
 
 
 # Application definition
