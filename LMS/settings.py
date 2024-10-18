1f03f92ad8664c709592af07d484163faafce53d
prasad456789 <iiittstudent755@gmail.com>
2024-10-18 16:07:07 +0530

    Update settings.py

--- LMS/settings.py
+++ LMS/settings.py
@@ -134,7 +134,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static')
+    os.path.join(BASE_DIR,'staticfiles')
 ]
 
 # Default primary key field type
